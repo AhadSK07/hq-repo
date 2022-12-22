3c606f46d13e3379d802f7fe199036b83d2c2b8c
ahadk69 <121217997+ahadk69@users.noreply.github.com>
2022-12-22 22:11:07 +0530

    Update keep-alive.py

--- keep-alive.py
+++ keep-alive.py
@@ -3,11 +3,11 @@ from requests import get as rget
 from os import environ
 from logging import error as logerror
 
-RENDER_EXTERNAL_URL = environ.get('RENDER_EXTERNAL_URL', '')
-if RENDER_EXTERNAL_URL != 0:
+BASE_URL = environ.get('BASE_URL')
+if BASE_URL:
     while True:
         try:
-            rget(RENDER_EXTERNAL_URL).status_code
+            rget(BASE_URL).status_code
             sleep(1200)
         except Exception as e:
             logerror(f"keep-alive.py: {e}")
